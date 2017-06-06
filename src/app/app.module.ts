8994d6178d62876dcd01749da9a7488c6dc921e1
Dmitry Kaufman <Kaufman.Dmitry@gmail.com>
2017-06-06 17:49:30 +0700

    Http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,32 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module'
+import { HttpModule }    from '@angular/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
+import { AppRoutingModule } from './app-routing.module'
 import { AppComponent }  from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
 import { HeroService }         from './hero.service';
 import { DashboardComponent }         from './dashboard.component';
+import { HeroSearchComponent }         from './hero-search.component';
 
 @NgModule({
-  imports:      [
+  imports: [
       BrowserModule,
       FormsModule,
-      AppRoutingModule
+      AppRoutingModule,
+      HttpModule,
+      InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   declarations: [
       AppComponent,
       HeroDetailComponent,
       HeroesComponent,
-      DashboardComponent
+      DashboardComponent,
+      HeroSearchComponent
   ],
   bootstrap:    [ AppComponent ],
     providers: [HeroService]
